e8724e0c6005e2c7856b8abf300df7c99b002c4b
Justin Starry <jstarry@users.noreply.github.com>
2019-06-17 19:25:36 -0400

    Implement create contract call and add tests

--- dead-mans-switch/runtime/src/dead_mans_switch.rs
+++ dead-mans-switch/runtime/src/dead_mans_switch.rs
@@ -1,13 +1,14 @@
 use super::SuperCall;
 use parity_codec::{Decode, Encode};
+use runtime_primitives::traits::As;
 use support::dispatch::{Dispatchable, Result};
-use support::{decl_event, decl_module, decl_storage, StorageValue};
+use support::{decl_event, decl_module, decl_storage, ensure, StorageMap, StorageValue};
 use system::{ensure_signed, RawOrigin};
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct Contract<AccountId, BlockNumber> {
-    benificiary: AccountId,
+    beneficiary: AccountId,
     block_delay: BlockNumber,
     execution_block: BlockNumber,
 }
@@ -33,14 +34,16 @@ decl_storage! {
     trait Store for Module<T: Trait> as DeadMansSwitchModule {
         Contracts: map T::AccountId => Contract<T::AccountId, T::BlockNumber>;
 
-        TrusteesArray get(trustees_by_index): map (T::AccountId, u64) => T::AccountId;
-        TrusteesCount get(trustees_count): map T::AccountId => u64;
-        TrusteesIndex: map T::AccountId => u64;
+        // Common way of implementing vectors with maps in substrate
+        TrustorsArray get(trustors_by_index): map (T::AccountId, u64) => T::AccountId;
+        TrustorsCount get(trustors_count): map T::AccountId => u64;
+        TrustorsIndex: map T::AccountId => u64;
+
+        MinBlockDelay: T::BlockNumber = T::BlockNumber::sa(10);
     }
 }
 
 decl_module! {
-    /// The module declaration.
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
         fn deposit_event<T>() = default;
@@ -58,8 +61,33 @@ decl_module! {
             Ok(())
         }
 
-        pub fn create_contract(origin, beneficiary: T::AccountId, delay: T::BlockNumber) -> Result {
-            unimplemented!()
+        pub fn create_contract(origin, beneficiary: T::AccountId, block_delay: T::BlockNumber) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            ensure!(!<Contracts<T>>::exists(&sender), "You can only have one contract");
+            ensure!(sender != beneficiary, "You cannot use yourself as your beneficiary");
+
+            let min_block_delay = <MinBlockDelay<T>>::get();
+            ensure!(block_delay >= min_block_delay, "Your block delay is too short");
+
+            let trustors_count = Self::trustors_count(&beneficiary);
+            let new_trustors_count = trustors_count.checked_add(1)
+                .ok_or("Overflow adding a new trustor for this beneficiary")?;
+
+            let current_block = <system::Module<T>>::block_number();
+            let execution_block = current_block + block_delay;
+            let contract = Contract {
+                beneficiary,
+                block_delay,
+                execution_block,
+            };
+            <Contracts<T>>::insert(&sender, &contract);
+
+            <TrustorsArray<T>>::insert((sender.clone(), trustors_count), &sender);
+            <TrustorsCount<T>>::insert(&contract.beneficiary, new_trustors_count);
+            <TrustorsIndex<T>>::insert(&sender, trustors_count);
+
+            Ok(())
         }
 
         pub fn update_beneficiary(origin, beneficiary: T::AccountId) -> Result {
@@ -84,7 +112,7 @@ mod tests {
         traits::{BlakeTwo256, IdentityLookup},
         BuildStorage,
     };
-    use support::{assert_ok, impl_outer_origin};
+    use support::{assert_noop, assert_ok, impl_outer_origin};
 
     impl_outer_origin! {
         pub enum Origin for Test {}
@@ -122,9 +150,10 @@ mod tests {
     impl Trait for Test {
         type Event = ();
     }
-    type DeadMansSwitchModule = Module<Test>;
 
-    fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+    type DMS = Module<Test>;
+
+    fn build_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
         let mut t = system::GenesisConfig::<Test>::default()
             .build_storage()
             .unwrap()
@@ -147,14 +176,53 @@ mod tests {
     }
 
     #[test]
-    fn it_works_for_default_value() {
-        with_externalities(&mut new_test_ext(), || {
+    fn act_as_should_work() {
+        with_externalities(&mut build_ext(), || {
             let super_call = SuperCall::Balances(balances::Call::transfer(0, 100));
-            assert_ok!(DeadMansSwitchModule::act_as(
-                Origin::signed(0),
-                1,
-                super_call
-            ));
+            assert_ok!(DMS::act_as(Origin::signed(0), 1, super_call));
+        });
+    }
+
+    #[test]
+    fn create_contract_should_work() {
+        with_externalities(&mut build_ext(), || {
+            // create a contract to give access to account #1 after 10 blocks of inactivity
+            assert_ok!(DMS::create_contract(Origin::signed(0), 1, 10));
+
+            // check that account #1 has one trustor
+            assert_eq!(DMS::trustors_count(1), 1);
+
+            // check that account #0 does not have a trustor
+            assert_eq!(DMS::trustors_count(0), 0);
+
+            // check that account #0 is trustor of account #1
+            assert_eq!(DMS::trustors_by_index((1, 0)), 0);
+        });
+    }
+
+    #[test]
+    fn create_contract_should_fail() {
+        with_externalities(&mut build_ext(), || {
+            // create a contract to give access to account #1 after 10 blocks of inactivity
+            assert_ok!(DMS::create_contract(Origin::signed(0), 1, 10));
+
+            // check that account cannot create another contract
+            assert_noop!(
+                DMS::create_contract(Origin::signed(0), 2, 10),
+                "You can only have one contract"
+            );
+
+            // check that short delay is disallowed
+            assert_noop!(
+                DMS::create_contract(Origin::signed(1), 2, 0),
+                "Your block delay is too short"
+            );
+
+            // check that account cannot set themselves as beneficiary
+            assert_noop!(
+                DMS::create_contract(Origin::signed(1), 1, 0),
+                "You cannot use yourself as your beneficiary"
+            );
         });
     }
 }
